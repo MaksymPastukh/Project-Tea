5102bfcce1ad28a2ac1fae444e601c79d479eb4f
MaksymPastukh <saponexamq@gmail.com>
2024-09-15 17:02:35 +0200

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainComponent} from "./components/pages/main/main.component";
+import {ProductComponent} from "./components/pages/product/product.component";
+import {OrderComponent} from "./components/pages/order/order.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'product', component: ProductComponent},
+  {path: 'order', component: OrderComponent},
+  {path: '**', redirectTo: ''},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/common/header/header.component';
+import { MainComponent } from './components/pages/main/main.component';
+import { OrderComponent } from './components/pages/order/order.component';
+import { FooterComponent } from './components/common/footer/footer.component';
+import { ProductComponent } from './components/pages/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    OrderComponent,
+    FooterComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
